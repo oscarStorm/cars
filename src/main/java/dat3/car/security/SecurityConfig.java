f0e66bc37941df8bd88239838bbf85981b4ed2ce
oscarstorm <oscar.emil.storm@gmail.com>
2023-03-07 12:26:34 +0100

    permitall

--- src/main/java/dat3/car/security/SecurityConfig.java
+++ src/main/java/dat3/car/security/SecurityConfig.java
@@ -92,13 +92,13 @@ public class SecurityConfig {
             //necessary to allow for "nice" JSON Errors
             .requestMatchers("/error").permitAll()
 
-            //.requestMatchers("/", "/**").permitAll());
+            .requestMatchers("/", "/**").permitAll());
 
            // .requestMatchers(HttpMethod.GET,"/api/demo/anonymous").permitAll());
 
            // Demonstrates another way to add roles to an endpoint
            // .requestMatchers(HttpMethod.GET, "/api/demo/admin").hasAuthority("ADMIN")
-    .anyRequest().authenticated());
+    //.anyRequest().authenticated());
 
     return http.build();
   }
